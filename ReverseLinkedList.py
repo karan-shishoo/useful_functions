921d9db7ccd8a83668c8a3b871e031cde906edc6
karan-shishoo <karan.shishoo@gmail.com>
2020-02-26 16:03:10 +0530

    Update ReverseLinkedList.py

--- ReverseLinkedList.py
+++ ReverseLinkedList.py
@@ -1,12 +1,19 @@
+#Setting up a rudimentary ListNode Class to build our LinkedList from
 class ListNode:
+  #Class only has 2 properties, the value the node holds and the next node in the list which defaults to None
   def __init__(self, value):
       self.value = value
       self.next = None
 
-
+#function to reverse the LinkedList
 def reverse_linked_list(head_node):
+  #check to see if a list even exists
+  if not head_node.next:
+    return head_node
+  #Save the head Node and next Node to use as the start to our loop
   current_node = head_node
   next_node = current_node.next
+  #make the current head Node the tail
   current_node.next = None
 
   while next_node.next:
@@ -19,6 +26,7 @@ def reverse_linked_list(head_node):
 
   return next_node
 
+#Function to print a LinkedList to check its state (only viable for small lists with str/int values)
 def print_nodes(head_node):
   current_node = head_node
   node_builder = ""
@@ -28,7 +36,7 @@ def print_nodes(head_node):
   node_builder += str(current_node.value)
   return node_builder
 
-
+#Creating a LinkedList to test the functions
 a = ListNode(1)
 b = ListNode(2)
 c = ListNode(3)
@@ -42,3 +50,4 @@ d.next = e
 
 print(print_nodes(a))
 print(print_nodes(reverse_linked_list(a)))
+#Made by Karan Shishoo
